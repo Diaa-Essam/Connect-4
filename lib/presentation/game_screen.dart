9c5dd16c99fb2453770ba299eaddd2870f85fe43
Diaa-Essam <studywithdod@gmail.com>
2026-04-09 09:28:55 +0200

    feat: connect UI grid with board state using controller

--- lib/presentation/game_screen.dart
+++ lib/presentation/game_screen.dart
@@ -1,7 +1,9 @@
+import 'package:connect_four/controller/game_controller.dart';
 import 'package:flutter/material.dart';
 
 class GameScreen extends StatelessWidget {
-  const GameScreen({super.key});
+  final GameController controller = GameController();
+  GameScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,12 +14,22 @@ class GameScreen extends StatelessWidget {
           crossAxisCount: 7,
         ),
         itemBuilder: (context, index) {
+          int row = index ~/ 7;
+          int col = index % 7;
+
+          int cell = controller.board.grid[row][col];
+
+          Color color;
+          if (cell == 1) {
+            color = Colors.red;
+          } else if (cell == 2) {
+            color = Colors.yellow;
+          } else {
+            color = Colors.grey;
+          }
           return Container(
             margin: EdgeInsets.all(4),
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.grey,
-            ),
+            decoration: BoxDecoration(shape: BoxShape.circle, color: color),
           );
         },
       ),
