47b78eacf1a9897c24d5bd8618e362a8f347843b
Diaa-Essam <studywithdod@gmail.com>
2026-04-09 07:57:00 +0200

    feat: add GameScreen in presentation layer

--- lib/controller/game_controller.dart
+++ lib/controller/game_controller.dart
@@ -4,14 +4,17 @@ class GameController {
   Board board = Board();
   int currentPlayer = 1;
 
-  void makeMove(int column) {
+  bool makeMove(int column) {
     bool success = board.dropPiece(column, currentPlayer);
+
+    if (!success) return false;
 
     if (success) {
       if (board.checkWin(currentPlayer)) {
-        print("Plyer $currentPlayer wins!");
+        print("Player $currentPlayer wins!");
       }
       currentPlayer = (currentPlayer == 1) ? 2 : 1;
     }
+    return true;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,8 @@
+import 'package:connect_four/presentation/game_screen.dart';
 import 'package:flutter/material.dart';
 import 'model/board.dart';
 
 void main() {
-  Board board = Board();
-  board.dropPiece(1, 1);
-  board.grid[0][0] = 1;
-  board.grid[0][1] = 1;
-  board.grid[0][2] = 1;
-  board.grid[0][3] = 1;
-  board.printBoard();
-  print(board.checkHorizontalWin(1));
   runApp(const MyApp());
 }
 
@@ -18,6 +11,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(home: GameScreen());
   }
 }
