d457d1a2f81366aa6d1c805f3574c47416839b77
Diaa-Essam <studywithdod@gmail.com>
2026-04-09 09:16:25 +0200

    feat: diaply 6x7 grid using Gridview builder

--- lib/presentation/game_screen.dart
+++ lib/presentation/game_screen.dart
@@ -5,6 +5,22 @@ class GameScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Game Board Here")));
+    return Scaffold(
+      body: GridView.builder(
+        itemCount: 42,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 7,
+        ),
+        itemBuilder: (context, index) {
+          return Container(
+            margin: EdgeInsets.all(4),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.grey,
+            ),
+          );
+        },
+      ),
+    );
   }
 }
