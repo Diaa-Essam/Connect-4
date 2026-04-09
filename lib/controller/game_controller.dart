880d7d1d37904bfb76575eeb3458c0aadefcf50f
Diaa-Essam <studywithdod@gmail.com>
2026-04-09 07:35:11 +0200

    feat: implement makeMove with turn switching and win detection

--- lib/controller/game_controller.dart
+++ lib/controller/game_controller.dart
@@ -3,4 +3,15 @@ import 'package:connect_four/model/board.dart';
 class GameController {
   Board board = Board();
   int currentPlayer = 1;
+
+  void makeMove(int column) {
+    bool success = board.dropPiece(column, currentPlayer);
+
+    if (success) {
+      if (board.checkWin(currentPlayer)) {
+        print("Plyer $currentPlayer wins!");
+      }
+      currentPlayer = (currentPlayer == 1) ? 2 : 1;
+    }
+  }
 }
